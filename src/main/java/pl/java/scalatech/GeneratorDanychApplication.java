4ee66bbc446dcf1a407d1dcf9a3e6413f083dc21
Sławek Borowiec <przodownik@tlen.pl>
2016-11-16 10:00:09 +0100

    cloud

--- src/main/java/pl/java/scalatech/GeneratorDanychApplication.java
+++ src/main/java/pl/java/scalatech/GeneratorDanychApplication.java
@@ -3,15 +3,10 @@ package pl.java.scalatech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.hawt.springboot.EnableHawtio;
-import io.hawt.web.AuthenticationFilter;
-
 @SpringBootApplication
-@EnableHawtio
 public class GeneratorDanychApplication {
 
-	public static void main(String[] args) {
-	    System.setProperty(AuthenticationFilter.HAWTIO_AUTHENTICATION_ENABLED, "false");
+	public static void main(String[] args) {	 
 		SpringApplication.run(GeneratorDanychApplication.class, args);
 	}
 }
--- src/main/java/pl/java/scalatech/camel/GenerateDataRoute.java
+++ src/main/java/pl/java/scalatech/camel/GenerateDataRoute.java
@@ -1,21 +1,23 @@
 package pl.java.scalatech.camel;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 @Component
+@Profile("myCamel")
 @Slf4j
 public class GenerateDataRoute extends RouteBuilder{
 
    @Override
     public void configure() throws Exception {
         from("timer://foo?fixedRate=true&period=2000").routeId("timerRoute")
-         .bean(UserGenerate.class).to("seda:throttle");
+         .bean(UserGenerate.class).to("log:myCamel");
    
-         from("seda:throttle").routeId("throttleMessageRoute").throttle(2).timePeriodMillis(20000).to("seda:nextQueue");
+ 
          
-         from("seda:nextQueue").routeId("sendToQueue").to("activemq:randomUser");
+ 
     }
 
     
